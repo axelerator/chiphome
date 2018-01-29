cfa91d4b9b9dbad8e5dc80c63028d11584d8f03b
Axel Tetzlaff <axel.tetzlaff@gmx.de>
2018-01-29 21:26:06 +0000

    wip

--- myapp.rb
+++ myapp.rb
@@ -1,10 +1,13 @@
 # myapp.rb
 require 'sucker_punch'
 require 'sinatra'
-
+set :bind, '0.0.0.0'
 DRY_MODE = false
 begin
   require 'chip-gpio'
+  PINS = ChipGPIO.get_pins
+  PINS[:XIO7].export
+  PINS[:XIO7].direction = :output
 rescue NameError => e
   DRY_MODE = true
 end
@@ -22,13 +25,21 @@ class GpioJob
       else
         puts "DISABLING #{pin_name}"
       end
+    else
+      if !!enable
+        PINS[pin_name].value = 1
+        puts "ENABLING #{pin_name}"
+      else
+        PINS[pin_name].value = 0
+        puts "DISABLING #{pin_name}"
+      end
     end
   end
 end
 
 class LivingRoomLigts < GpioJob
   def pin_name
-    "XIO_7"
+    :XIO7
   end
 end
 
@@ -38,12 +49,12 @@ get '/' do
 end
 
 post '/lights/on' do
-  LivingRoomLigts.perform_in(5, GpioJob::ON)
+  LivingRoomLigts.perform_in(1, GpioJob::ON)
   redirect '/dashboard.html?flash=lightson'
 end
 
 
 post '/lights/off' do
-  LivingRoomLigts.perform_in(5, GpioJob::OFF)
+  LivingRoomLigts.perform_in(1, GpioJob::OFF)
   redirect '/dashboard.html?flash=lightsoff'
 end
